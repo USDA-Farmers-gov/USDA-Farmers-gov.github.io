d8d9675b5a74473649b4a43297b2c91006d19414
Matt Calvert <mcalvert@metrostarsystems.com>
2019-08-01 15:32:46 -0400

    fixing links

--- components/Menu.vue
+++ components/Menu.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <a :href="'/'">Home</a>
-    | <a :href="'/elements/button'">Button</a>
-    | <a :href="'/elements/colors'">Colors</a>
-    | <a :href="'/elements/typography'">Typography</a>
+    | <a :href="'/elements/Button'">Button</a>
+    | <a :href="'/elements/Colors'">Colors</a>
+    | <a :href="'/elements/Typography'">Typography</a>
   </div>
 </template>
