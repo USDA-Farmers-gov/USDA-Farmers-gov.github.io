f9d058b137f67a83e77a06fc0b7f84b9aebf2a2a
Matt Calvert <mcalvert@metrostarsystems.com>
2019-08-01 14:04:36 -0400

    rebuilding files from script

--- publish-site.sh
+++ publish-site.sh
@@ -6,4 +6,5 @@
 npm run generate
 git add .
 git commit -m "rebuilding files from script"
-git subtree push --prefix dist origin master
+git push
+git subtree push --prefix=dist https://github.com/mcalvert2089/mcalvert2089.github.io master
--- push-doc-pages.sh
+++
@@ -1,6 +0,0 @@
-npm run generate
-rm -rf docs
-cp -r dist docs
-git add .
-git commit -m "generating doc files"
-git push
