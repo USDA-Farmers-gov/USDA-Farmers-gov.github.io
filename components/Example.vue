c3ff81d4ecf088b1673e06adb182f1b36b464758
Matt Calvert <mcalvert@metrostarsystems.com>
2019-08-12 16:49:20 -0400

    fixed problem with example component

--- components/Example.vue
+++ components/Example.vue
@@ -6,12 +6,9 @@
     <div v-html="markup" class="example-container"></div>
 
     <p v-if="code.background || code.card || code.checkbox || code.text" class="text-medium">
-      <span v-if="code.background" class="label">BACKGROUND</span>
-      <span v-if="code.card" class="label">CARD</span>
-      <span v-if="code.checkbox" class="label">CHECKBOX</span>
-      <span v-if="code.text" class="label">TEXT</span>
+      <span class="label">{{ setContentHeader() }}</span>
       <br/>
-      <span class="text-medium" v-html="addLineBreaks(code.background)"></span>
+      <span class="text-medium" v-html="setCode()"></span>
     </p>
   </div>
 </template>
@@ -32,6 +29,26 @@
     methods: {
       setFlexClass() {
         return 'medium-' + 12/Number(this.columns)
+      },
+      setContentHeader() {
+        let header = ''
+
+        if(this.code.background) header = 'BACKGROUND'
+        if(this.code.card) header = 'CARD'
+        if(this.code.checkbox) header = 'CHECKBOX'
+        if(this.code.text) header = 'TEXT'
+
+        return header
+      },
+      setCode() {
+        let code = ''
+
+        if(this.code.background) code = this.code.background
+        if(this.code.card) code = this.code.card
+        if(this.code.checkbox) code = this.code.checkbox
+        if(this.code.text) code = this.code.text
+
+        return utils.addLineBreaks(code)
       },
       addLineBreaks(text) {
         return utils.addLineBreaks(text)
