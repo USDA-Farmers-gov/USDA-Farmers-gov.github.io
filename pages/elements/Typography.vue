6f44518b4f8b10a13d0c06372d9e0e6609cbc747
Matt Calvert <mcalvert@metrostarsystems.com>
2019-08-13 21:02:47 -0400

    rebuilding files from script

--- pages/elements/Typography.vue
+++ pages/elements/Typography.vue
@@ -33,7 +33,7 @@
       </thead>
       <tbody>
         <tr v-for="font in public_sans">
-          <td v-html="font.name"></td>
+          <td v-html="font.name_and_markup"></td>
           <td>{{ font.font_family }}</td>
           <td>{{ font.weight }}</td>
           <td>{{ font.font_size }}</td>
@@ -57,7 +57,7 @@
           </td>
         </tr>
         <tr v-for="font in merriweather">
-          <td v-html="font.name"></td>
+          <td v-html="font.name_and_markup"></td>
           <td>{{ font.font_family }}</td>
           <td>{{ font.weight }}</td>
           <td>{{ font.font_size }}</td>
@@ -161,21 +161,21 @@ export default {
   data() {
     return {
       public_sans: [
-        { name: '<h1>Headline 1</h1>', font_family: 'Public Sans', weight: '700', font_size: '38px', line_height: '48px' },
-        { name: '<h2>Headline 2</h2>', font_family: 'Public Sans', weight: '700', font_size: '28px', line_height: '38px' },
-        { name: '<h3>Headline 3</h3>', font_family: 'Public Sans', weight: '700', font_size: '23px', line_height: '30px' },
-        { name: '<h4>Headline 4</h4>', font_family: 'Public Sans', weight: '700', font_size: '20px', line_height: '26px' },
-        { name: '<h5>Headline 5</h5>', font_family: 'Public Sans', weight: '700', font_size: '16px', line_height: '20px' },
-        { name: '<h6>Headline 6</h6>', font_family: 'Public Sans', weight: '400', font_size: '12px', line_height: '18px' },
-        { name: '<div class="label">Label 1</div>', font_family: 'Public Sans', weight: '700', font_size: '22px', line_height: '34px' },
-        { name: '<div class="lead-p">Lead Paragraph</div>', font_family: 'Public Sans', weight: '300', font_size: '22px', line_height: '34px' },
-        { name: '<div class="body-p">Body Copy</div>', font_family: 'Public Sans', weight: '400', font_size: '18px', line_height: '28px' },
-        { name: '<div class="subheading">Subheading</div>', font_family: 'Public Sans', weight: '400', font_size: '16px', line_height: '24px' },
-        { name: '<div class="caption">Caption*</div>', font_family: 'Public Sans', weight: '400', font_size: '14px', line_height: '22px' },
+        { name_and_markup: '<h1>Headline 1</h1>', font_family: 'Public Sans', weight: '700', font_size: '38px', line_height: '48px' },
+        { name_and_markup: '<h2>Headline 2</h2>', font_family: 'Public Sans', weight: '700', font_size: '28px', line_height: '38px' },
+        { name_and_markup: '<h3>Headline 3</h3>', font_family: 'Public Sans', weight: '700', font_size: '23px', line_height: '30px' },
+        { name_and_markup: '<h4>Headline 4</h4>', font_family: 'Public Sans', weight: '700', font_size: '20px', line_height: '26px' },
+        { name_and_markup: '<h5>Headline 5</h5>', font_family: 'Public Sans', weight: '700', font_size: '16px', line_height: '20px' },
+        { name_and_markup: '<h6>Headline 6</h6>', font_family: 'Public Sans', weight: '400', font_size: '12px', line_height: '18px' },
+        { name_and_markup: '<div class="label">Label 1</div>', font_family: 'Public Sans', weight: '700', font_size: '22px', line_height: '34px' },
+        { name_and_markup: '<div class="lead-p">Lead Paragraph</div>', font_family: 'Public Sans', weight: '300', font_size: '22px', line_height: '34px' },
+        { name_and_markup: '<div class="body-p">Body Copy</div>', font_family: 'Public Sans', weight: '400', font_size: '18px', line_height: '28px' },
+        { name_and_markup: '<div class="subheading">Subheading</div>', font_family: 'Public Sans', weight: '400', font_size: '16px', line_height: '24px' },
+        { name_and_markup: '<div class="caption">Caption*</div>', font_family: 'Public Sans', weight: '400', font_size: '14px', line_height: '22px' },
       ],
       merriweather: [
-        { name: '<div class="blog-body-p">Blog Body Copy*</div>', font_family: 'Merriweather', weight: '400', font_size: '18px', line_height: '30px' },
-        { name: '<div class="blog-block-quote">Blog Quote*</div>', font_family: 'Merriweather', weight: '300', font_size: '28px', line_height: '34px' },
+        { name_and_markup: '<div class="blog-body-p">Blog Body Copy*</div>', font_family: 'Merriweather', weight: '400', font_size: '18px', line_height: '30px' },
+        { name_and_markup: '<div class="blog-block-quote">Blog Quote*</div>', font_family: 'Merriweather', weight: '300', font_size: '28px', line_height: '34px' },
       ],
       contrast_buttons_normal: [
         { classes: 'heritage heritage-border white-txt', description: 'White on Heritage Green'},
