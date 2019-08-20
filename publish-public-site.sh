cd5cb91af4ba34e6cc2b28da5d9a285b119f0552
Matt Calvert <mcalvert@metrostarsystems.com>
2019-08-20 16:09:07 -0400

    updated publish script

--- publish-public-site.sh
+++ publish-public-site.sh
@@ -2,14 +2,16 @@
 # Before running this script for the first time, run this command 
 # to add the public site repository:
 # git subtree add --prefix=dist https://github.com/mcalvert2089/mcalvert2089.github.io master
+#
+# Changes in the app must be committed to the repo before you can rebuild the site.
+# This is to prevent the commit message for the Github site from being used in the app repo.
 
-#npm run generate
-#rm dist/README.md
 if [ -z "$(git status --porcelain)" ]; then 
-  echo "up to date"
+  npm run generate
+  rm dist/README.md
+  git add .
+  git commit -m "website rebuilt"
+  git subtree push --prefix=dist https://github.com/mcalvert2089/mcalvert2089.github.io master
 else 
   echo "ERROR: Please commit app updates before rebuilding site."
 fi
-#git add .
-#git commit -m "website rebuilt"
-#git subtree push --prefix=dist https://github.com/mcalvert2089/mcalvert2089.github.io master
