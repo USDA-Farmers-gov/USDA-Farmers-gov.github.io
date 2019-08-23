f0b5064ce5926ad75e832324b76f4a6ac46f7c36
Matt Calvert <mcalvert@metrostarsystems.com>
2019-08-23 11:18:22 -0400

    website rebuilt

--- assets/js/utils.js
+++
@@ -1,7 +0,0 @@
-const utils = {
-  addLineBreaks(text) {
-      return text ? text.trim().replace(/(?:\r\n|\r|\n)/g, '<br>') : '';
-    }
-}
-
-export default utils
--- plugins/global.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-import Code from '../components/Code.vue'
-import ExampleRow from '../components/ExampleRow.vue'
-import ColorSwatches from '../components/ColorSwatches.vue'
-
-Vue.component('Code', Code)
-Vue.component('ExampleRow', ExampleRow)
-Vue.component('ColorSwatches', ColorSwatches)
