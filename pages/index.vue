fd142c260c36c6dfa6d5e77f1438031c11a977cc
Matt Calvert <mcalvert@metrostarsystems.com>
2019-08-23 15:46:02 -0400

    added placeholder text to homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,15 @@
 <template>
-  
+  <div>
+    <h2>Design</h2>
+    <p class="intro-text">
+      Farmers.gov offers a flexible yet coherent visual style you can apply to your work. Our visual style offers a clean, modern aesthetic that meets high standards of usability and accessibility while promoting credibility and trust.
+    </p>
+
+    <h3>Accessibility</h3>
+    <p>
+      All of our designs meet the WCAG 2.0 AA accessibility guidelines and are compliant with Section 508 of the Rehabilitation Act. If you choose to customize these designs, please make sure they continue to meet the requirements listed in the “Accessibility” section of each design.
+    </p>
+  </div>
 </template>
 
 <script>
