2046db2d1dd7e8fe3a7e285db58973fffe0510c6
Matt Calvert <mcalvert@metrostarsystems.com>
2019-08-23 15:42:52 -0400

    removed content from index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,5 @@
 <template>
-  <div>
-    <h1>Overview</h1>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sed nisi eu orci fermentum congue non sit amet ipsum. Nunc in euismod ipsum, quis tincidunt magna. Sed ut magna id erat ultrices facilisis et ac lacus. Sed interdum vehicula lacus ac dignissim. Donec eget molestie nulla. Nulla turpis nulla, hendrerit a convallis et, suscipit a ipsum. Nam congue arcu in neque placerat fermentum. Donec blandit venenatis mi, sed rhoncus nunc cursus non.
-    </p>
-    <p>
-      Maecenas non interdum diam, sed tincidunt lorem. In hac habitasse platea dictumst. Praesent a tincidunt mauris. Curabitur efficitur hendrerit dui sed malesuada. Nulla quis venenatis ex, a finibus erat. Cras sit amet eros euismod est cursus sagittis a quis nisi. Aenean sagittis quis ipsum eu molestie. Aenean a sagittis lectus. Sed tincidunt bibendum pellentesque. Sed vitae rhoncus lectus.
-    </p>
-  </div>
+  
 </template>
 
 <script>
