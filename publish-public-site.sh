b2df4750dd45e7cb19e33eb3828530e892e9e12c
Matt Calvert <mcalvert@metrostarsystems.com>
2019-08-29 15:07:04 -0400

    updated comments

--- publish-public-site.sh
+++ publish-public-site.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
-# Before running this script for the first time, run this command 
+# Before running this script on a new repo for the first time, run this command 
 # to add the public site repository:
 # git subtree add --prefix=dist https://github.com/mcalvert2089/mcalvert2089.github.io master
 #
 # Changes in the app must be committed to the repo before you can rebuild the site.
-# This is to prevent the commit message for the Github site from being used in the app repo.
+# This is to prevent the commit message for the Github site from being used in the Nuxt app repo.
 
 if [ -z "$(git status --porcelain)" ]; then 
   npm run generate
