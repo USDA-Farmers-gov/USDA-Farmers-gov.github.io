4974d9f1766f34db0c05b17ad08fe2317326c00b
Matt Calvert <mcalvert@metrostarsystems.com>
2019-09-03 14:35:11 -0400

    more updates to code component

--- components/Code.vue
+++ components/Code.vue
@@ -41,17 +41,19 @@
           var indentBefore = new Array(level++ + 1).join('  '),
               indentAfter  = new Array(level - 1).join('  '),
               textNode
-          
+
           for (var i = 0; i < node.children.length; i++) {
               textNode = document.createTextNode('\n' + indentBefore)
               node.insertBefore(textNode, node.children[i])
+
+              this.formatHTML(node.children[i], level)
               
               if (node.lastElementChild === node.children[i]) {
                   textNode = document.createTextNode('\n' + indentAfter)
                   node.appendChild(textNode)
               }
           }
-          
+
           return node
       }
     }
