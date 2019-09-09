edb2341641d82ef794e26e407158ec8d17924f8e
Matt Calvert <mcalvert@metrostarsystems.com>
2019-09-09 15:42:13 -0400

    added links page

--- pages/ui/Links.vue
+++ pages/ui/Links.vue
@@ -11,6 +11,56 @@
     <p>
       <a href="#">Text Link</a>
     </p>
+
+    [ LINK EXAMPLES ]
+
+    <h3>External Text Link</h3>
+
+    <p>
+      External links are used when directing users to a web page outside of Farmers.gov and USDA. An external link should appear with an external link icon to distinguish between Farmers.gov and USDA to non-Farmers.gov and USDA web pages. They should open in a new browser tab.
+    </p>
+
+    [ EXTERNAL LINK SINGLE ]
+    <br>
+    [ EXTERNAL LINK EXAMPLES ]
+
+    <h3>Inline Textual Link</h3>
+    <p>
+      Inline textual links are used within body paragraphs. They are underlined on default so they can be easily distinguished between body text.
+    </p>
+
+    [ INLINE LINK SINGLE ]
+    <br>
+    [ INLINE LINK EXAMPLES ]
+
+    <h3>Anchor Link</h3>
+    <p>
+      Anchor links are used to navigate users to another section within the same page. They can be used with or without a leaf icon.
+    </p>
+
+    [ ANCHOR LINK SINGLE ]
+    <br>
+    [ ANCHOR LINK EXAMPLES ]
+
+    <h4>Click Target</h4>
+    <p>
+      The click target includes the link text and its associated icon. This applys to all link styles.
+    </p>
+
+    [ LINK HIGHLIGHT ]
+
+    <h4>Accessibility</h4>
+    <p>
+      Color cannot be used as the only means of conveying information, functionality, or prompting a response. It must be paired with another visual indicator such as underlining, bolding, or other visual cues.
+    </p>
+    <p>
+      Hover and focus states must be incorporated to convey to users that these elements are clickable. These visual effects should be device-independent and able to be activated by either a mouse or keyboard.
+    </p>
+
+    <h4>References</h4>
+    <a href="https://www.w3.org/TR/WCAG21/#distinguishable">https://www.w3.org/TR/WCAG21/#distinguishable</a>
+    <br>
+    <a href="https://webaim.org/techniques/hypertext/link_text">https://webaim.org/techniques/hypertext/link_text</a>
   </div>
 </template>
 
