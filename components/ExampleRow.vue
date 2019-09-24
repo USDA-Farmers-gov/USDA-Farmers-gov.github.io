e4533a1e52c256c28a711f9088596b76e2c875be
Matt Calvert <mcalvert@metrostarsystems.com>
2019-09-24 09:09:27 -0400

    path changes

--- components/ExampleRow.vue
+++ components/ExampleRow.vue
@@ -16,7 +16,7 @@
 </template>
 
 <script>
-  import utils from '~/assets/js/utils'
+  import utils from '@/assets/js/utils'
 
   export default {
     props: {
--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,7 @@ export default {
   ** Plugins to load before mounting the App
   */
   plugins: [
-    '~/plugins/global.js'
+    '@/plugins/global.js'
   ],
   /*
   ** Nuxt.js dev-modules
