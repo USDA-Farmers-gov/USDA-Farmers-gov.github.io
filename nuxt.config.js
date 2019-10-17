fc32b7f6be1b915863f6d79398376ed405cc9c01
Matt Calvert <mcalvert@metrostarsystems.com>
2019-10-17 09:27:15 -0400

    fixed responsiveness on table page

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,7 @@ export default {
     },
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
       { name: 'robots', content: 'noindex'}
     ],
