94ba57acb3704e71f622c05fc7f4a35c57fc20ad
Matt Calvert <mcalvert@metrostarsystems.com>
2019-10-18 11:53:06 -0400

    disabling publish script until demo is over

--- publish-public-site.sh
+++ publish-public-site.sh
@@ -6,11 +6,13 @@
 # Changes in the app must be committed to the repo before you can rebuild the site.
 # This is to prevent the generic commit message for the Github site from being used in the Nuxt app repo.
 
+echo "DISABLED FOR DEMO"
+
 # if [ -z "$(git status --porcelain)" ]; then 
-  npm run generate
-  git add .
-  git commit -m "website rebuilt"
-  git subtree push --prefix=dist https://github.com/mcalvert2089/mcalvert2089.github.io master
+#  npm run generate
+#  git add .
+#  git commit -m "website rebuilt"
+#  git subtree push --prefix=dist https://github.com/mcalvert2089/mcalvert2089.github.io master
 # else 
 #  echo "ERROR: Please commit app updates before rebuilding site."
 # fi
