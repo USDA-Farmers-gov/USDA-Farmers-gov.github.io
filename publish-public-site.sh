bfca02c5c93abb37dec5be862131220098eaf2e9
Matt Calvert <mcalvert@metrostarsystems.com>
2019-10-24 09:15:49 -0400

    updated publish script

--- publish-public-site.sh
+++ publish-public-site.sh
@@ -6,13 +6,33 @@
 # Changes in the app must be committed to the repo before you can rebuild the site.
 # This is to prevent the generic commit message for the Github site from being used in the Nuxt app repo.
 
+  publish() {
+    npm run generate
+    git add .
+    git commit -m "website rebuilt"
+    git subtree push --prefix=dist https://github.com/USDA-Farmers-gov/USDA-Farmers-gov.github.io master
+  }
 
-# if [ -z "$(git status --porcelain)" ]; then 
-  npm run generate
-  git add .
-  git commit -m "website rebuilt"
-  git subtree push --prefix=dist https://github.com/USDA-Farmers-gov/USDA-Farmers-gov.github.io master
-#  git subtree push --prefix=dist https://github.com/mcalvert2089/mcalvert2089.github.io master
-# else 
-#  echo "ERROR: Please commit app updates before rebuilding site."
-# fi
+  status=$(git status --porcelain)
+
+  if [ ! -z "$status" ]; then 
+    printf "\e[1;37;41mCurrent branch not clean.\e[0m\n"
+    echo ''
+    echo 'Git status message:'
+    echo '==================='
+    echo "$status"
+    echo ''
+
+    while true; do
+      read -p "Publish anyways (y|n)?" yn
+        case $yn in
+            [Yy]* ) publish; break;;
+            [Nn]* ) exit;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+  fi
+
+ publish;
+
+
